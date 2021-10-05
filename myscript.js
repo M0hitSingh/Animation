6ef82d5c56cc2386bd00a5b4a6fd4c74dac810d2
golchi <golchigeek@gmail.com>
2021-10-06 00:43:07 +0530

    updated script fxn

--- myscript.js
+++ myscript.js
@@ -1,7 +1,18 @@
-function change()
-    var image = [];
-    image[0] = 'img/dance2.png';
-    for(var i = 0 ; i < 5 ; i++){
-        document.getElementById('1') = image[i];
+var img = ["img/dance1.png","img/dance2.png","img/dance3.png","img/dance4.png","img/dance5.png"];
+var t =300;
+var i =0;
+function dance_img(){
+    // for(var i =0 ; ; i++){
+    //     document.getElementById("dd").src = img[i];    
+    // }
+    document.getElementById("dd").src = img[i];
+    i++;
+    setTimeout("dance_img()",t);
+    if(i+1>img.length){
+        i=0;
     }
+    // document.getElementById("dd").src = img[0];
+}
+function stop(){
+    location.reload();
 }
